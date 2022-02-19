b768636e9c560f1ce043ba4e763df51077c5253f
Swayam tejas padhy <swayamtejaspadhy1234@gmail.com>
2022-02-19 09:10:35 +0530

    e

--- rice2.go
+++ rice2.go
@@ -3,6 +3,14 @@ package main
 import (
   "fmt"
 )
+
+/*
+Raca condition is when multiple threads are trying to access and manipulat the same variable.
+In the code below, main, add_one and sub_one are all accessing and changing the value of x.
+Due to the uncertainty of Goroutine scheduling mechanism, the results of the following program is unpredictable.
+*/
+
+
 func add_one(pt *int) {
   (*pt)++
   fmt.Println(*pt)
